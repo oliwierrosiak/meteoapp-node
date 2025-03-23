637f3dbbd60fb0a031b62f89ea9168bdf957371a
Oliwier <oliwierrosiak07@gmail.com>
2025-03-24 00:02:36 +0100

    Sending push fixed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import './express/expressConfig.js'
 import dotenv from "dotenv"
 import { app } from './express/expressConfig.js'
+import sendNotification from './services/sendNotification.js'
 
 dotenv.config()
+
 
 app.listen(process.env.port,()=>{
     console.log(`serwer słucha na porcie ${process.env.port}`)
--- services/sendNotification.js
+++ services/sendNotification.js
@@ -1,33 +1,26 @@
+import '../services-account.json' assert { type: 'json' };
+import { Expo } from 'expo-server-sdk'
+
+const expo = new Expo();
+
+
 async function sendNotification(weatherInfo,expoToken)
 {
-    const notification = {
-        to: expoToken,
-        sound: "default",
-        title:"Nowe powiadomienie!",
-        body:"To jest testowe powiadomienie push"
-      };
-
-    // console.log(weatherInfo)
+  console.log(expoToken)
+  let messages = [];
+  messages.push({
+    to: expoToken,
+    sound: 'default',
+    title: 'Tytuł powiadomienia',
+    body: 'Treść powiadomienia',
+  });
     
-    try
-    {
-        const response = await fetch("https://exp.host/--/api/v2/push/send", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Accept-Encoding": "gzip, deflate",
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(notification)
-        });
-    
-        const data = await response.json();
-        console.log(data)
-    }
-    catch(ex)
-    {
-        console.log(ex)
-    }
+  try {
+    const response = await expo.sendPushNotificationsAsync(messages);
+    console.log('Push notification sent successfully:', response);
+  } catch (error) {
+    console.error('Error sending push notification:', error);
+  }
 
 }
 
